f5d2c4e846d2e4d3c30f2ce8b678fb28eac940c3
pzif <rodrigooliveira0407@gmail.com>
2022-12-16 16:57:05 -0300

    projeto comleto

--- temtudo/views.py
+++ temtudo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'temtudo/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+        return context
+
+
+class FiltersView(TemplateView):
+    template_name = 'temtudo/filters.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'filters'
+        return context
